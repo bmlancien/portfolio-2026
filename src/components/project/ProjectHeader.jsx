5e9ece2dcb5886f577ea8ae6c9326a10bf2b3ab3
Bryan Lancien <bryanlancien.ui@gmail.com>
2026-04-10 18:35:06 +0100

    Improve typopgraphy on project pages

--- src/components/project/ProjectHeader.jsx
+++ src/components/project/ProjectHeader.jsx
@@ -2,21 +2,17 @@ import React from 'react';
 
 export default function ProjectHeader({ organization, funding, years, title, skills }) {
   return (
-    <div className="max-w-8xl mx-auto px-6 lg:px-12 pt-24 lg:pt-32 pb-12">
+    <div className="max-w-8xl mx-auto px-6 lg:px-12 pt-12 lg:pt-18 xl:pt-24 lg:pt-32 pb-12">
       <div className="flex flex-wrap items-center gap-1 text-sm lg:text-base xl:text-xl text-gray-500 mb-2 xl:mb-6">
-        <span>{organization}</span>
-        <span className="mx-2">·</span>
-        <span>{funding}</span>
-        <span className="mx-2">·</span>
-        <span>{years}</span>
+        <span>{organization} · {funding} · {years}</span>
       </div>
-      <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold text-gray-800 font-rajdhani leading-tight mb-4 xl:mb-8 max-w-7xl">
+      <h1 className="text-3xl md:text-5xl lg:text-6xl font-bold text-gray-800 font-rajdhani leading-tight mb-4 xl:mb-8 max-w-7xl">
         {title}
       </h1>
       <div className="flex flex-wrap items-center gap-3">
         <span className="text-base lg:text-lg xl:text-2xl text-gray-800">Role & Skills:</span>
         {skills.map((skill) => (
-          <span key={skill} className="px-4 py-1 bg-gray-100 text-gray-800 rounded-full text-base lg:text-lg xl:text-2xl">
+          <span key={skill} className="px-4 py-1 bg-gray-100 text-gray-800 rounded-full text-sm lg:text-base xl:text-xl">
             {skill}
           </span>
         ))}
