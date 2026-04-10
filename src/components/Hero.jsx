fa38fbef59d4f843bf1e61c304a8b1e812710806
Bryan Lancien <bryanlancien.ui@gmail.com>
2026-04-10 18:41:01 +0100

    Improve typography on hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ const Hero = () => {
     <section className="bg-white">
       <div className="max-w-8xl flex justify-center mx-auto px-6 lg:px-8 xl:px-12 py-8 md:py-16 lg:py-20 pt-16 lg:pt-24 xl:pt-32">
         <div className="max-w-5xl flex flex-col items-center">
-          <h1 className="text-center text-4xl md:text-5xl lg:text-6xl xl:text-8xl font-bold font-rajdhani text-gray-700 mb-4 leading-tight">
+          <h1 className="text-center text-5xl md:text-6xl lg:text-7xl xl:text-8xl font-bold font-rajdhani text-gray-700 mb-4 leading-tight">
             Data-Heavy Interfaces for Climate Tech
           </h1>
           <p className="text-center text-lg md:text-xl lg:text-2xl xl:text-3xl font-light text-gray-700">
